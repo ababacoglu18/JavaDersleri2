187c832c451e415d1cea90b9e89cc3a00e464a48
ALTAN BABAÇOĞLU <81329584+ababacoglu18@users.noreply.github.com>
2022-09-09 21:18:51 +0300

    Packages1

---
+++ PackagesGiris/src/com/altanb/deneme/Main.java
@@ -0,0 +1,18 @@
+package com.altanb.deneme;
+
+
+import com.altanb.swingornegi.Window;
+
+import javax.swing.*;
+
+public class Main {
+
+
+    public static void main(String[] args){
+
+            Window window1 = new Window();
+            JFrame frame = new JFrame();
+
+
+    }
+}
---
+++ PackagesGiris/src/com/altanb/swingornegi/Window.java
@@ -0,0 +1,28 @@
+package com.altanb.swingornegi;
+
+import javax.swing.*;
+
+public class Window {
+
+
+    private JFrame frame ;
+
+    public Window(){
+
+        frame = new JFrame();
+
+        JButton button = new JButton("Click on Here");
+        button.setBounds(130,100,100,40);
+        frame.add(button);
+
+        frame.setSize(400,500);
+        frame.setLayout(null);
+        frame.setVisible(true);
+
+        
+
+
+
+    }
+
+}
