e089d200ce4731304c6b96cbe93d6ea1a339c65e
ALTAN BABAÇOĞLU <81329584+ababacoglu18@users.noreply.github.com>
2022-12-15 18:13:25 +0300

    Hashmap2

---
+++ HashMap/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        HashMap<Integer, String> hashMap = new HashMap<Integer,String>();
+        hashMap.put(10,"Java"); // Add yok. Collection Framework'ten Extendsli calismiyor. 
+        //.
+
+
+    }
+}
