f394bf2e4ffe422dd88c7adc5f1019c3a7a212b6
ALTAN BABAÇOĞLU <81329584+ababacoglu18@users.noreply.github.com>
2022-12-18 19:56:22 +0300

    Exception Handling try inner catch finally

--- ExceptionHandling/InnerFunctionsExceptionHandling/src/Main.java
+++ ExceptionHandling/InnerFunctionsExceptionHandling/src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
+
+
+    public static void UcuncuFonksiyon(){
+        int a;
+
+              System.out.println(a=12/0);
+
+    }
+    public static void IkinciFonksiyon(){
+       try{ UcuncuFonksiyon();}
+       catch (ArithmeticException e){
+
+               System.out.println("Bir sayi 0'a bolunemez.");  //Stack içinde herhangi bir yerde Exception yakalanmak zorundadır. 
+
+       }
+    }
+    public static void BirinciFonksiyon(){
+        IkinciFonksiyon();
+    }
+
+    //**************************************************************************\\
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BirinciFonksiyon();
+
     }
 }
