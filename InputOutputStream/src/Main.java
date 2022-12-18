307f230aa9961fa650b24dbed94e1c48c8c312fa
ALTAN BABAÇOĞLU <81329584+ababacoglu18@users.noreply.github.com>
2022-12-18 20:26:51 +0300

    e

--- InputOutputStream/src/Main.java
+++ InputOutputStream/src/Main.java
@@ -1,15 +1,47 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
         FileOutputStream fl1 = null;
         File file = new File("dosya.txt");
+
+
         try {
-            fl1 = new FileOutputStream(file);
+            fl1 = new FileOutputStream(file,true); //True üzerine appendle yazmaya devam et.
         } catch (FileNotFoundException e) {
+            System.out.println("Dosya Bulunamadı");
+
+            try {
+
+                String s = "Altan B";
+
+                byte[] s_byte = s.getBytes();
+
+                fl1.write(65);
+                fl1.write(65);
+                fl1.write(65);
+                fl1.write(65);
+                fl1.write(65);
+                fl1.write(65);
+                fl1.write(s_byte);
+
+            } catch (IOException ex) {
+                System.out.println("dosyaya yazilmadi.");
+            }
+
             throw new RuntimeException(e);
+        }
+
+        finally {
+
+            try {
+                fl1.close();
+            } catch (IOException e) {
+                System.out.println("Dosya Kapatılırken Hata oluştu.");;
+            }
         }
     }
 }
