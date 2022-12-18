4becd1b8c343f6c5178847c3898be314edc85e60
ALTAN BABAÇOĞLU <81329584+ababacoglu18@users.noreply.github.com>
2022-12-18 21:18:02 +0300

    dd

--- InputOutputStream/src/Main.java
+++ InputOutputStream/src/Main.java
@@ -1,47 +1,87 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+
+
 
 public class Main {
     public static void main(String[] args) {
-        FileOutputStream fl1 = null;
-        File file = new File("dosya.txt");
 
+        FileOutputStream fos = null;
+        //File file =  new File("dosya.txt");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Loglara hangi bilgi eklensin?");
+
+        String a  = scanner.nextLine();
 
         try {
-            fl1 = new FileOutputStream(file,true); //True üzerine appendle yazmaya devam et.
-        } catch (FileNotFoundException e) {
-            System.out.println("Dosya Bulunamadı");
+            fos = new FileOutputStream("dosya.txt"); //True Uzerine yazmaya devam etmesini saglar.
+
+            //byte[] array = {101,75,66,68};
+            String s = "AltanB";
+            if (a.equals("q")){
+                fos.flush();
+
+            } else {
+
+
+               for(int i = 0 ; i<1;i++)
+               {byte[] s_array = a.getBytes();
+
+
+                fos.write(s_array);}
+
+            }
+
+        } catch (FileNotFoundException ex) {
+            System.out.println("File Not found exception oluştu....");
+        } catch (IOException ex) {
+            System.out.println("Dosyaya yazılırken bir hata oluştu...");
+        }
+        finally{
 
             try {
-
-                String s = "Altan B";
-
-                byte[] s_byte = s.getBytes();
-
-                fl1.write(65);
-                fl1.write(65);
-                fl1.write(65);
-                fl1.write(65);
-                fl1.write(65);
-                fl1.write(65);
-                fl1.write(s_byte);
-
+                fos.close();
             } catch (IOException ex) {
-                System.out.println("dosyaya yazilmadi.");
+                System.out.println("Dosya kapatılırken bir hata oluştu...");
             }
 
-            throw new RuntimeException(e);
         }
 
-        finally {
+        // File Input Stream
+            FileInputStream fis  = null;
 
+        try {
+         fis   = new FileInputStream("dosya.txt");    ///OKUMA ALGORITMASI ONEMLI
+
+         int deger;
+         String s = "";
+         while ((deger=fis.read()) != -1){
+             s += (char)deger;
+         }
+            System.out.println("Dosya Icerigi: " + s );
+
+            //System.out.println(  "Okunan Karakter: " +(char)(fis.read()));
+
+
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Dosya Bulunamadi.");;
+        } catch (IOException e) {
+            System.out.println("Dosya Okunamadı.");
+        } finally {
+            if(fis != null){
             try {
-                fl1.close();
+                fis.close();
             } catch (IOException e) {
-                System.out.println("Dosya Kapatılırken Hata oluştu.");;
-            }
+                System.out.println("Dosya Kapatilamadi.");;
+            }}
         }
+
+
     }
 }
