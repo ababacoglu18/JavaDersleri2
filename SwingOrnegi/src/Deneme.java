9f54e123d77b672e37517f954b04f267c0e3ee85
ALTAN BABAÇOĞLU <81329584+ababacoglu18@users.noreply.github.com>
2022-12-27 21:27:39 +0300

    Swing

---
+++ SwingOrnegi/src/Deneme.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Deneme {
+    private JButton sal;
+    private JPanel panel1;
+    private JTextField selam;
+    private JFormattedTextField formattedTextField1;
+public Deneme() {
+    sal.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            selam.setText("Ahmet");
+        }
+    });
+}
+
+    public static class Main {
+        public static void main(String[] args) {
+
+            
+
+        }
+    }
+}
