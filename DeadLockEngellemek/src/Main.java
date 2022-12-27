6abb7cfeebb2ca8f610971d687f80d3bad342f52
ALTAN BABAÇOĞLU <81329584+ababacoglu18@users.noreply.github.com>
2022-12-27 19:36:04 +0300

    deadlock

---
+++ DeadLockEngellemek/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        //Kavşakta sıkışan arabalar buna örnek verilebilir.
+        //Trafik Kilitlenir
+        //Birçok thread ayni keyi alirsa ortaya cikar
+
+
+
+
+
+    }
+}
